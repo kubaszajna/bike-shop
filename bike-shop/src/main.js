8ffaaa67fc3cabe500ab9fecbfc3b747a5bd7eb5
kubaszajna <kubaszajna@gmail.com>
2020-07-02 23:55:44 +0200

    Clean template

--- bike-shop/src/main.js
+++ bike-shop/src/main.js
@@ -1,6 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import jQuery from 'jquery';
+
+window.$ = window.jQuery = jQuery;
+
+import 'bootstrap';
+import 'popper.js';
+import './assets/app.scss';
 
 Vue.config.productionTip = false;
 
