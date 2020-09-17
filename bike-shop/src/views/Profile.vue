567c62ddf998b130c62060375ced5500f376fc1c
kubaszajna <kubaszajna@gmail.com>
2020-09-17 21:16:11 +0200

    Profile page structure

--- bike-shop/src/views/Profile.vue
+++ bike-shop/src/views/Profile.vue
@@ -1,16 +1,120 @@
 <template>
-  <div class="orders">
-    <div class="container h-100">
+  <div class="products">
+    <div class="container">
       <div class="intro h-100">
-        <div class="row h-100 justify-content-center align-items-center">
-          <div class="col-md-6">
-            <h3>Orders Page</h3>
+        <div class="row h-100 align-items-center">
+          <div class="col-md-6 ml-3">
+            <h3>Profile settings</h3>
+
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              Change your profile settings here
             </p>
           </div>
-          <div class="col-md-6">
-            <img src="../assets/orders.svg" alt="overview image" class="img-fluid" />
+          <div class="col-md-5">
+            <img src="/img/svg/profile.svg" width="300" alt="" class="img-fluid" />
+          </div>
+        </div>
+      </div>
+
+      <div class="profile-content">
+        <ul class="nav nav-pills ml-3" id="myTab" role="tablist">
+          <li class="nav-item">
+            <a class="nav-link active" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="true">Profile</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" id="account-tab" data-toggle="tab" href="#account" role="tab" aria-controls="account" aria-selected="false">Account settings</a>
+          </li>
+        </ul>
+
+        <div class="tab-content" id="myTabContent">
+          <div class="tab-pane fade show active pt-3" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <input type="text" name="" placeholder="Full name" class="form-control" />
+                  </div>
+                </div>
+
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <input type="text" placeholder="Phone" class="form-control" />
+                  </div>
+                </div>
+
+                <div class="col-md-12">
+                  <div class="form-group">
+                    <input type="text" placeholder="Address" class="form-control" />
+                  </div>
+                </div>
+
+                <div class="col-md-8">
+                  <div class="form-group">
+                    <input type="text" placeholder="Postcode" class="form-control" />
+                  </div>
+                </div>
+
+                <div class="col-md-4">
+                  <div class="form-group">
+                    <input type="submit" value="Save Changes" class="btn btn-primary w-100" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="tab-pane fade pt-3" id="account" role="tabpanel" aria-labelledby="account-tab">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-">
+                  <div class="alert alert-info">
+                    Please use the Reset password email button for reseting the password. The form doens't work currently
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <input type="text" placeholder="User name" class="form-control" />
+                  </div>
+                </div>
+
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <input type="text" placeholder="Email address" class="form-control" />
+                  </div>
+                </div>
+
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <input type="text" placeholder="New password" class="form-control" />
+                  </div>
+                </div>
+
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <input type="text" placeholder="Confirm password" class="form-control" />
+                  </div>
+                </div>
+
+                <div class="col-md-4">
+                  <div class="form-group">
+                    <input type="file" class="form-control" />
+                  </div>
+                </div>
+
+                <div class="col-md-4">
+                  <div class="form-group">
+                    <input type="submit" value="Save Changes" class="btn btn-primary w-100" />
+                  </div>
+                </div>
+
+                <div class="col-md-4">
+                  <div class="form-group">
+                    <input type="button" value="Reset password email" class="btn btn-success w-100" />
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
