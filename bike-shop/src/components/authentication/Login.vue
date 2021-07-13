9949dd8c8afc215b94f7d3a46811ea21d2c48fba
Jakub Szajna <j.szajna@ideo.pl>
2021-07-14 00:32:46 +0200

    Improve modal tabs

--- bike-shop/src/components/authentication/Login.vue
+++ bike-shop/src/components/authentication/Login.vue
@@ -1,72 +1,55 @@
 <template>
     <div class="login">
         <div class="modal">
-            <div class="modal-container" v-show="currentTab === 0">  
-                    <ul class="modal-tabs">
-                        <li v-for="(tab, index) in tabs" :key="index"
-                        @click="currentTab = index"
-                        :class="currentTab === index ? 'active' : ''"
-                        class="link small">{{ tab }}
-                        </li>
-                    </ul>
-                    <div class="modal-left">
-                        <h1 class="modal-title">Welcome! <br>Login please.</h1>
-                        <p class="modal-desc">Test user data: <span>admin@administrator.com / admin123</span></p>
-                        <div class="input-block">
-                            <label for="email" class="input-label">Email</label>
-                            <input type="email" v-model="emailLogin" id="emailLogin" placeholder="Email">
-                        </div>
-                        <div class="input-block">
-                            <label for="password" class="input-label">Password</label>
-                            <input type="password" v-model="passwordLogin" id="passwordLogin" placeholder="Password">
-                        </div>
-                        <div class="modal-buttons">
-                        <a href="" class="">Forgot your password?</a>
-                        <button type="button" @click="login" class="link small">Login</button>
-                        </div>
-                        <p class="sign-up">Don't have an account? <button class="link small" @click="register">Sign up</button></p>
+            <div class="modal-container">  
+                <ul class="modal-tabs">
+                    <li v-for="(tab, index) in tabs" :key="index"
+                    @click="currentTab = index"
+                    :class="currentTab === index ? 'active' : ''"
+                    class="link small">{{ tab }}
+                    </li>
+                </ul>
+                <div class="modal-left" v-show="currentTab === 0">
+                    <h1 class="modal-title">Welcome! <br>Login please.</h1>
+                    <p class="modal-desc">Test user data: <span>admin@administrator.com / admin123</span></p>
+                    <div class="input-block">
+                        <label for="email" class="input-label">Email</label>
+                        <input type="email" v-model="emailLogin" id="emailLogin" placeholder="Email">
                     </div>
-                    <div class="modal-right">
-                        <img src="../../assets/images/bike-theme/bike-login-signup.jpg" alt="Dark bike">
+                    <div class="input-block">
+                        <label for="password" class="input-label">Password</label>
+                        <input type="password" v-model="passwordLogin" id="passwordLogin" placeholder="Password">
                     </div>
-                    <button class="icon-button close-button">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
-                            <path d="M 25 3 C 12.86158 3 3 12.86158 3 25 C 3 37.13842 12.86158 47 25 47 C 37.13842 47 47 37.13842 47 25 C 47 12.86158 37.13842 3 25 3 z M 25 5 C 36.05754 5 45 13.94246 45 25 C 45 36.05754 36.05754 45 25 45 C 13.94246 45 5 36.05754 5 25 C 5 13.94246 13.94246 5 25 5 z M 16.990234 15.990234 A 1.0001 1.0001 0 0 0 16.292969 17.707031 L 23.585938 25 L 16.292969 32.292969 A 1.0001 1.0001 0 1 0 17.707031 33.707031 L 25 26.414062 L 32.292969 33.707031 A 1.0001 1.0001 0 1 0 33.707031 32.292969 L 26.414062 25 L 33.707031 17.707031 A 1.0001 1.0001 0 0 0 32.980469 15.990234 A 1.0001 1.0001 0 0 0 32.292969 16.292969 L 25 23.585938 L 17.707031 16.292969 A 1.0001 1.0001 0 0 0 16.990234 15.990234 z"></path>
-                        </svg>
-                    </button>
-            </div>
-            <div class="modal-container" v-show="currentTab === 1">
-                    <ul class="modal-tabs">
-                        <li v-for="(tab, index) in tabs" :key="index"
-                        @click="currentTab = index"
-                        :class="currentTab === index ? 'active' : ''"
-                        class="link small">{{ tab }}</li>
-                    </ul>
-                    <div class="modal-left">
-                        <h1 class="modal-title modal-title-create">Create New Account</h1>
-                        <div class="input-block">
-                            <label for="name" class="input-label">Your name</label>
-                            <input type="text" v-model="nameSignUp" id="nameSignUp" placeholder="Your name">
-                        </div>
-                        <div class="input-block">
-                            <label for="email" class="input-label">Email</label>
-                            <input type="email" v-model="emailSignUp" id="emailSignUp" placeholder="Email">
-                        </div>
-                        <div class="input-block">
-                            <label for="password" class="input-label">Password</label>
-                            <input type="password" v-model="passwordSignUp" id="passwordSignUp" placeholder="Password">
-                        </div>
-                        <p class="sign-up"><button class="link small" @click="register">Sign up</button></p>
+                    <div class="modal-buttons">
+                    <a href="" class="">Forgot your password?</a>
+                    <button type="button" @click="login" class="link small">Login</button>
                     </div>
-                    <div class="modal-right">
-                        <img src="../../assets/images/bike-theme/bike-login-signup.jpg" alt="Dark bike">
+                    <p class="sign-up">Don't have an account? <button class="link small" @click="register">Sign up</button></p>
+                </div>
+                <div class="modal-left" v-show="currentTab === 1">
+                    <h1 class="modal-title modal-title-create">Create New Account</h1>
+                    <div class="input-block">
+                        <label for="name" class="input-label">Your name</label>
+                        <input type="text" v-model="nameSignUp" id="nameSignUp" placeholder="Your name">
                     </div>
-                    <button class="icon-button close-button">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
-                            <path d="M 25 3 C 12.86158 3 3 12.86158 3 25 C 3 37.13842 12.86158 47 25 47 C 37.13842 47 47 37.13842 47 25 C 47 12.86158 37.13842 3 25 3 z M 25 5 C 36.05754 5 45 13.94246 45 25 C 45 36.05754 36.05754 45 25 45 C 13.94246 45 5 36.05754 5 25 C 5 13.94246 13.94246 5 25 5 z M 16.990234 15.990234 A 1.0001 1.0001 0 0 0 16.292969 17.707031 L 23.585938 25 L 16.292969 32.292969 A 1.0001 1.0001 0 1 0 17.707031 33.707031 L 25 26.414062 L 32.292969 33.707031 A 1.0001 1.0001 0 1 0 33.707031 32.292969 L 26.414062 25 L 33.707031 17.707031 A 1.0001 1.0001 0 0 0 32.980469 15.990234 A 1.0001 1.0001 0 0 0 32.292969 16.292969 L 25 23.585938 L 17.707031 16.292969 A 1.0001 1.0001 0 0 0 16.990234 15.990234 z"></path>
-                        </svg>
-                    </button>
-                
+                    <div class="input-block">
+                        <label for="email" class="input-label">Email</label>
+                        <input type="email" v-model="emailSignUp" id="emailSignUp" placeholder="Email">
+                    </div>
+                    <div class="input-block">
+                        <label for="password" class="input-label">Password</label>
+                        <input type="password" v-model="passwordSignUp" id="passwordSignUp" placeholder="Password">
+                    </div>
+                    <p class="sign-up"><button class="link small" @click="register">Sign up</button></p>
+                </div>
+                <div class="modal-right">
+                    <img src="../../assets/images/bike-theme/bike-login-signup.jpg" alt="Dark bike">
+                </div>
+                <button class="icon-button close-button">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
+                        <path d="M 25 3 C 12.86158 3 3 12.86158 3 25 C 3 37.13842 12.86158 47 25 47 C 37.13842 47 47 37.13842 47 25 C 47 12.86158 37.13842 3 25 3 z M 25 5 C 36.05754 5 45 13.94246 45 25 C 45 36.05754 36.05754 45 25 45 C 13.94246 45 5 36.05754 5 25 C 5 13.94246 13.94246 5 25 5 z M 16.990234 15.990234 A 1.0001 1.0001 0 0 0 16.292969 17.707031 L 23.585938 25 L 16.292969 32.292969 A 1.0001 1.0001 0 1 0 17.707031 33.707031 L 25 26.414062 L 32.292969 33.707031 A 1.0001 1.0001 0 1 0 33.707031 32.292969 L 26.414062 25 L 33.707031 17.707031 A 1.0001 1.0001 0 0 0 32.980469 15.990234 A 1.0001 1.0001 0 0 0 32.292969 16.292969 L 25 23.585938 L 17.707031 16.292969 A 1.0001 1.0001 0 0 0 16.990234 15.990234 z"></path>
+                    </svg>
+                </button>
             </div>
         </div>
     </div>
